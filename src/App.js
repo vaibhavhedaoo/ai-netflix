37773d4eb2f445273068599be720a65747e11263
vaibhavhedaoo <vaibhavhedaoo12@gmail.com>
2024-02-24 21:46:16 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-extrabold text-center text-4xl text-red-700">
+     AI-NetFlix
+    </div>
+  );
+}
+
+export default App;
